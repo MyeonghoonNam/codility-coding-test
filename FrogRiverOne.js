77026039a9a968f7a7d089ecb1845fff13e65f38
MyeongHoonNam <codenmh0822@naver.com>
2023-01-19 18:22:00 +0900

    문제 해결

---
+++ FrogRiverOne.js
@@ -0,0 +1,13 @@
+function solution(X, A) {
+  let set = new Set();
+
+  for (let i = 0; i < A.length; i++) {
+    set.add(A[i]);
+
+    if (set.size === X) {
+      return i;
+    }
+  }
+
+  return -1;
+}
